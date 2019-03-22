152202168dde1be59124430beb71ad7509231bb2
fatemiryan2019 <42677922+fatemiryan2019@users.noreply.github.com>
2019-03-22 08:43:55 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
         ArrayList<Account> accounts = new ArrayList<Account>();
         Scanner scan = new Scanner(System.in);
